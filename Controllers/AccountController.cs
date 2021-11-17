5b120caf5847f9a379cd683e4f0928e1d4b20b83
Nameson Gaudel <namesongaudel.ng@gmail.com>
2021-11-17 21:58:31 +0545

    Identity management setup

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using AppoinmentScudeler.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppoinmentScudeler.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
