ac5705a778d5f411f68e89e1eb9f03c82bc283fb
Nameson Gaudel <namesongaudel.ng@gmail.com>
2021-11-19 17:54:11 +0545

    Login view is settup with the login view model

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -11,7 +11,7 @@ namespace AppoinmentScudeler.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
