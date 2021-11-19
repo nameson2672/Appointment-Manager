46584db470838b37ca4a5313621cb07f35fb2971
Nameson Gaudel <namesongaudel.ng@gmail.com>
2021-11-19 20:08:58 +0545

    Refister view is added and LogIn and Register page is added to Home page

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -15,5 +15,9 @@ namespace AppoinmentScudeler.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
---
+++ Utility/Helper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace AppoinmentScudeler.Utility
+{
+    public static class Helper
+    {
+        public static string Admin = "Admin";
+        public static string Patient = "Pataient";
+        public const string Doctor = "Doctor";
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Value = Helper.Admin, Text=Helper.Admin},
+                new SelectListItem{Value = Helper.Patient, Text=Helper.Patient},
+                new SelectListItem{Value = Helper.Doctor, Text=Helper.Doctor},
+            };
+        }
+    }
+}
